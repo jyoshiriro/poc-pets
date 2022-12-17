6b02495240dc87e3d9d9d5eee0bcdf474bd5d939
José Yoshiriro <Jose.Yoshiriro@arc-one.com>
2022-12-17 00:18:58 -0300

    melhoras gerais

--- testes-api-pets/src/test/java/prosper/pets/resttest/get/CachorrosApiGetV1Test.java
+++ testes-api-pets/src/test/java/prosper/pets/resttest/get/CachorrosApiGetV1Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.blankOrNullString;
 
 public class CachorrosApiGetV1Test {
 
@@ -16,11 +17,28 @@ public class CachorrosApiGetV1Test {
     }
 
     @Test
+    @DisplayName("GET /pets sem autorização status 403")
+    void testGetSemAutorizacao() {
+        given().auth().basic("hacker","hacker").get("/pets").then()
+                .statusCode(403)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
     @DisplayName("GET /pets com conteúdo status 200 e application/json")
     void testGetComConteudo() {
         given().auth().basic("usuario1","s1").get("/pets").then()
                 .statusCode(200)
                 .contentType(ContentType.JSON);
+    }
+
+    @Test
+    @DisplayName("GET /pets sem conteúdo status 204 e sem corpo")
+    void testGetSemConteudo() {
+        given().auth().basic("usuario1", "s1").param("nome", "miojo")
+                .get("/pets").then().log().all()
+                .statusCode(204)
+                .body(blankOrNullString());
     }
 
 }
